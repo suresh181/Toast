e7e42bf578ce42d24f6e79c08738eb4e537f8873
B Suresh <80473048+suresh181@users.noreply.github.com>
2021-10-26 08:53:40 +0530

    Add files via upload

---
+++ src/main/java/com/example/toast/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.toast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Toast(this).apply {
+                duration = Toast.LENGTH_LONG
+                view = layoutInflater.inflate(R.layout.custom_toast,cltoast)
+                show()
+            }
+
+        }
+    }
+}
